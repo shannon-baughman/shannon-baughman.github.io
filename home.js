e9af3ea5936bc9148c7fa972ebb9207cac97f86b
andrewmthomas87 <andrewmthomas87@gmail.com>
2014-10-18 17:27:40 -0600

    Vertically centered h1 with JavaScript

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 
 function resize() {
 	$('body').css('font-size', $(window).width() / 100);
+	var h1 = $('section#main h1');
+	h1.css('padding-top', $(window).height() / 2 - h1.height());
 }
 
 $(window).resize(resize);
