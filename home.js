76e15277688c4b302352fbbe7bebab985a5f4ea1
andrewmthomas87 <andrewmthomas87@gmail.com>
2014-10-18 18:58:33 -0600

    Updated JavaScript h1 positioning code to work with line-height

--- home.js
+++ home.js
@@ -2,7 +2,7 @@
 function resize() {
 	$('body').css('font-size', $(window).width() / 100);
 	var h1 = $('section#main h1');
-	h1.css('padding-top', $(window).height() / 2 - h1.height());
+	h1.css('padding-top', $(window).height() / 2 - (h1.height() * 0.75));
 }
 
 $(window).resize(resize);
